5d4c2d77e2e938d10bb0cfa4b1eb766594b6cde4
Your Name <you@example.com>
2022-07-31 21:27:25 +0800

    2022-7-31 21:27

--- src/pages/pancake/index.style.ts
+++ src/pages/pancake/index.style.ts
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import { Row, Column } from 'components/flex';
+import { Column } from 'components/flex';
 
 export const Container = styled(Column)`
   height: 100vh;
