ca69318724a9164fbb3a4973c17807dca6165ed9
Your Name <you@example.com>
2022-08-02 10:25:29 +0800

    2022-8-2 10:25

--- src/pages/pancake/index.tsx
+++ src/pages/pancake/index.tsx
@@ -3,27 +3,15 @@ import { PageHeader } from './page-header';
 import './styles/index.scss';
 import { MenuProps } from 'antd';
 import { NavBarItems } from './consts/consts';
-import { useState } from 'react';
 
 export const Pancake = () => {
   const items: MenuProps['items'] = NavBarItems;
-  const [current, setCurrent] = useState('');
-  const onClick: MenuProps['onClick'] = (e) => {
-    if (e.key) {
-      setCurrent(e.key);
-    }
-  };
+
   return (
     <Container>
       <PageHeader></PageHeader>
       <NavBarContainer>
-        <NavBar
-          onClick={onClick}
-          defaultSelectedKeys={['Swap']}
-          selectedKeys={[current]}
-          mode='horizontal'
-          items={items}
-        />
+        <NavBar defaultSelectedKeys={['Swap']} mode='horizontal' items={items} />
       </NavBarContainer>
       <Main>main</Main>
       <Footer>footer</Footer>
